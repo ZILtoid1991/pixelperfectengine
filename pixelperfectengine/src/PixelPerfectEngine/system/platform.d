dd21b227d913326153713605da47c481fab59f24
ZILtoid1991 <laszloszeremi@outlook.com>
2018-12-22 04:02:25 +0100

    Add missing file
    platform.d was missing due to some issue. Hopefully this indirect upload will fix it

---
+++ pixelperfectengine/src/PixelPerfectEngine/system/platform.d
@@ -0,0 +1,20 @@
+module PixelPerfectEngine.system.platform;
+
+version(X86){
+	static enum ARCH_INTEL_X86 = true;
+	version(LDC){
+		static enum USE_INTEL_INTRINSICS = true;	///Enables the use of intel intrinsics in compilers that support it;
+	}else{
+		static enum USE_INTEL_INTRINSICS = false;
+	}
+}else version(X86_64){
+	static enum ARCH_INTEL_X86 = true;
+	version(LDC){
+		static enum USE_INTEL_INTRINSICS = true;	///Enables the use of intel intrinsics in compilers that support it;
+	}else{
+		static enum USE_INTEL_INTRINSICS = false;
+	}
+}else{
+	static enum ARCH_INTEL_X86 = false;
+	static enum USE_INTEL_INTRINSICS = false;
+}
