3dd0b0acca6d62f55e15489aa3402ab3b24f5435
ziltoid1991 <laszloszeremi@outlook.com>
2021-07-11 23:14:21 +0200

    Added some common audio functions

--- pixelperfectengine/src/PixelPerfectEngine/audio/base/func.d
+++ pixelperfectengine/src/PixelPerfectEngine/audio/base/func.d
@@ -14,16 +14,30 @@ import inteli.emmintrin;
 	/**
 	 * Mixes an audio stream to the destination.
 	 */
-	public void mixIntoStream(size_t length, float* src, float* dest, float amount = 1.0) {
+	public void mixIntoStream(size_t length, float* src, float* dest, float amount = 1.0, float corr = 0.5) {
 		const __m128 amountV = __m128([amount, amount, amount, amount]);
+		const __m128 corrV = __m128([corr, corr, corr, corr]);
 		while (length) {
 			const __m128 srcV = _mm_load_ps(src);
 			__m128 destV = _mm_load_ps(dest);
-			destV += srcV;
-			destV *= amountV;
+			destV += srcV * amountV;
+			destV *= corrV;
 			_mm_store_ps(dest, destV);
 			length -= 4;
 			src += 4;
 			dest += 4;
+		}
+	}
+	/**
+	 * Interleaves two channels.
+	 */
+	public void interleave(size_t length, float* srcL, float* srcR, float* dest) {
+		while (length) {
+			dest[0] = *srcL;
+			dest[1] = *srcR;
+			dest += 2;
+			srcL++;
+			srcR++;
+			length--;
 		}
 	}
