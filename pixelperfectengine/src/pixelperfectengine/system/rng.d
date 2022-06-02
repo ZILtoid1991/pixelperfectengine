30974bcc15ec54e103ec61488b64ad5a341f6c70
ZILtoid1991 <laszloszeremi@outlook.com>
2022-06-02 22:10:03 +0200

    Added random number generator

---
+++ pixelperfectengine/src/pixelperfectengine/system/rng.d
@@ -0,0 +1,29 @@
+module pixelperfectengine.system.rng;
+
+/** 
+ * Implements a pseudo-random number generator using a 64 bit Fibonacci LFSR.
+ *
+ * Example uses:
+ * * `seed() % probabilityOfEvent`
+ * * `(seed() % 100) > probabilityOfEvent`
+ */
+public struct RandomNumberGenerator {
+	protected ulong     reg;
+	/** 
+	 * Creates the LFSR, and initializes it by calling its seed function 64 times.
+	 */
+	public this (ulong reg) @nogc @safe pure nothrow {
+		this.reg = reg;
+		for (int i ; i < 64 ; i++)
+			seed();
+	}
+	/** 
+	 * Shuffles the register's content, then returns it.
+	 */
+	public ulong seed() @nogc @safe pure nothrow {
+		const ulong bit = 
+				~(reg ^ (reg >> 51) ^ (reg >> 53) ^ (reg >> 54) ^ (reg >> 55) ^ (reg >> 59) ^ (reg >> 62) ^ (reg >> 63));
+		reg = (reg>>1) | (bit<<63);
+		return reg;
+	}
+}
