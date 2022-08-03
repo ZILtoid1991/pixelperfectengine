1cd62a6d2f1c572656f312ec39cc40fb19c81885
ZILtoid1991 <laszloszeremi@outlook.com>
2022-08-03 23:43:24 +0200

    Work in implementing module tester/editor

--- test1/moduleeditor.d
+++ test1/moduleeditor.d
@@ -1,27 +1,54 @@
 import pixelperfectengine.concrete.window; 
+import pixelperfectengine.audio.base.modulebase;
 import std.conv : to;
 
 public class ModuleEditor : Window {
 	ListView listView_presets;
 	ListView listView_values;
-	//Button button_remove;
-	//Button button_add;
-	//Button button_export;
 	SmallButton[] smallButtons;
+	//CheckBox	checkBox_globals;
 	public this(dstring name) {
 		super(Box(0, 0, 330, 330), name ~ " presets"d);
 		listView_presets = new ListView(new ListViewHeader(16, [32, 32, 240], ["Bank" ,"Prg" ,"Name"]), null, 
 				"listView_presets", Box(5, 20, 230, 100));
 		listView_values = new ListView(new ListViewHeader(16, [200, 100], ["" ,"listView1" ,""]), null, "listView_values", 
 				Box(5, 105, 325, 325));
-		//button_remove = new Button("Remove"d, "button_remove", Box(235, 80, 290, 100));
-		//button_add = new Button("Add"d, "button_add", Box(235, 55, 290, 75));
-		//button_export = new Button("Export"d, "button_export", Box(235, 30, 290, 50));
+		smallButtons ~= new SmallButton("loadB", "loadA", "load", Box(236, 20, 241, 35));
+		smallButtons ~= new SmallButton("saveB", "saveA", "save", Box(242, 20, 257, 35));
+		smallButtons ~= new SmallButton("importB", "importA", "import", Box(258, 20, 273, 36));
+		smallButtons ~= new SmallButton("exportB", "exportA", "export", Box(274, 20, 289, 36));
+
+		smallButtons ~= new SmallButton("addB", "addA", "add", Box(236, 36, 241, 51));
+		smallButtons ~= new SmallButton("removeB", "removeA", "remove", Box(242, 36, 257, 51));
+		smallButtons ~= new SmallButton("globalsB", "globalsA", "globals", Box(258, 36, 273, 51));
+		smallButtons ~= new SmallButton("macroB", "macroA", "macro", Box(274, 36, 289, 51));
+
+		listView_presets.editEnable = true;
 		addElement(listView_presets);
+		listView_presets.onItemSelect = &listView_presets_onSelect;
+		listView_presets.onTextInput = &listView_presets_onTextEdit;
+		listView_values.editEnable = true;
 		addElement(listView_values);
-		//smallButtons ~= new SmallButton();
-		//addElement(button_remove);
-		//addElement(button_add);
-		//addElement(button_export);
+		listView_values.onItemSelect = &listView_values_onSelect;
+		listView_values.onTextInput = &listView_values_onTextEdit;
+		foreach (SmallButton key; smallButtons) {
+			addElement(key);
+			key.onMouseLClick = &smallButtons_onClick;
+		}
+	}
+	protected void smallButtons_onClick(Event ev) {
+		SmallButton sender = cast(SmallButton)ev.sender;
+	}
+	protected void listView_presets_onSelect(Event ev) {
+
+	}
+	protected void listView_presets_onTextEdit(Event ev) {
+
+	}
+	protected void listView_values_onSelect(Event ev) {
+
+	}
+	protected void listView_values_onTextEdit(Event ev) {
+
 	}
 }
