5e72d7a5fe9175fe553a7822b4f6ee07effd6d2c
ZILtoid1991 <laszloszeremi@outlook.com>
2022-10-16 23:09:23 +0200

    Creating testcase for ETML text parsing

---
+++ test5/app.d
@@ -0,0 +1,22 @@
+module test5.app;
+
+import pixelperfectengine.graphics.outputscreen;
+import pixelperfectengine.graphics.raster;
+import pixelperfectengine.graphics.layers;
+
+import pixelperfectengine.graphics.bitmap;
+import pixelperfectengine.graphics.text;
+import pixelperfectengine.graphics.draw;
+
+import pixelperfectengine.system.lang.textparser;
+
+import pixelperfectengine.system.input;
+import pixelperfectengine.system.file;
+
+int main(string[] args) {
+
+}
+
+public class Test5 {
+
+}
