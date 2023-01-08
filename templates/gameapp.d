6bbf0a93fc95a9fe3ee89cd064b0e18f70ac019a
ZILtoid1991 <laszloszeremi@outlook.com>
2023-01-08 23:06:26 +0100

    Adding second template

---
+++ templates/gameapp.d
@@ -0,0 +1,7 @@
+/*
+This is a template for creating games with PixelPerfectEngine.
+Contains some basic setup, but needs further work to be fully featured besides of adding some game logic.
+ */
+
+module templates.gameapp;
+
