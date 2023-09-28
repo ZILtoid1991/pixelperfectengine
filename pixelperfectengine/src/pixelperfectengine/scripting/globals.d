69868ae642a0c8bb8a63308fd000c8bbc984640e
ZILtoid1991 <laszloszeremi@outlook.com>
2023-09-28 10:28:26 +0200

    update globals.d

--- pixelperfectengine/src/pixelperfectengine/scripting/globals.d
+++ pixelperfectengine/src/pixelperfectengine/scripting/globals.d
@@ -6,6 +6,12 @@ import pixelperfectengine.audio.base.handler;
 
 import collections.hashmap;
 
+///Contains the main raster to be used with scripting engines.
 public Raster mainRaster;
+///Contains the main module manager to be used with scripting engines.
 public ModuleManager modMan;
+///The bitmap resource manager for scripting purposes.
+///Any bitmap to be used by a scripting engine, especially if said scripting engine is an
+///external entity (DLL, etc), should be stored here to keep a reference around to avoid 
+///them being destroyed by the garbage collector.
 public HashMap!(string, ABitmap) scrptResMan;
