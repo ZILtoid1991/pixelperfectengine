e1e46c7b189777b5e6088b1c358dc0318ce2a621
ZILtoid1991 <laszloszeremi@outlook.com>
2024-11-05 23:13:37 +0100

    Upgrading template to new engine version

--- templates/gameapp.d
+++ templates/gameapp.d
@@ -10,7 +10,7 @@ module templates.gameapp;
 import std.stdio;
 import std.typecons : BitFlags;					//This is a good way to create bitflags, so you don't need to keep track of a bunch of boolean values.
 
-import bindbc.sdl;								//As of now, this is needed to initialize the SDL API. Might be changed in the future
+import bindbc.opengl;							//As of now, OpenGL is only being used to display the CPU framebuffer, but it'll change in the near future
 //Graphics related imports
 import pixelperfectengine.graphics.outputscreen;//Needed to display the final graphics
 import pixelperfectengine.graphics.raster;		//Needed to display layers in order
@@ -39,12 +39,11 @@ import pixelperfectengine.map.mapformat;
 ///Our main function, needed for the program to operate.
 ///You can add `string[] args` if your either really need or really want.
 int main() {
-	initialzeSDL();						//Initializes the SDL subsystem, so we will have input and graphics
 	try {								//A try-catch block to handle any errors. A bit ugly, but can save us when there's issues with debug symbols, or an error happened outside of a D code
 		GameApp app = new GameApp();
 		app.whereTheMagicHappens();
 	} catch (Throwable e) {
-		writeln(e);
+		debug writeln(e);
 	}
 	return 0;
 }
@@ -65,10 +64,13 @@ public class GameApp : SystemEventListener, InputListener {
 		left		=   1<<2,
 		right		=   1<<3,
 	}
+	enum SCREEN_WIDTH = 424;	///Width of the game screen, in pixels
+	enum SCREEN_HEIGHT = 240;	///Height of the game screen, in pixels
+	enum SCREEN_SCALE = 4;		///Initial screen scaling
 	///Stores the currently loaded map file with all related data.
 	MapFormat		mapSource;
-	///To display our game's graphics
-	OutputScreen	output;
+	///To display our game's graphics, we need to initialize an output window, which will also allow us to get input from the real world
+	OSWindow		outputScreen;
 	///To manage our layers and palette.
 	Raster			rstr;
 	///For input handling.
@@ -99,9 +101,12 @@ public class GameApp : SystemEventListener, InputListener {
 	/// Put other things here if you need them.
 	this () {
 		stateFlags.isRunning = true;	//Sets the state to running, so the main loop will stay running.
-		output = new OutputScreen("Your app name here", 424 * 4, 240 * 4);	//Creates an output window with the display size of 1696x960.
-		rstr = new Raster(424,240,output,0);//Creates a raster with the size of 424x240.
-		output.setMainRaster(rstr);		//Sets the main raster of the output screen.
+		outputScreen = new OSWindow("Your app name here", SCREEN_WIDTH * SCREEN_SCALE, SCREEN_HEIGHT * SCREEN_SCALE,
+				WindowCfgFlags.IgnoreMenuKey);	//Creates an output window with the display size calculated from various enums.
+		output.getOpenGLHandle();		//Initialize OpenGL
+		const glStatus = loadOpenGL();	//Load the OpenGL symbols
+		assert (glStatus >= GLSupport.gl11, "OpenGL not found!");	//Error out if openGL does not work
+		rstr = new Raster(SCREEN_WIDTH,SCREEN_HEIGHT,outputScreen,0);//Creates a raster with the size determined by the enums.
 
 		ih = new InputHandler();		//Creates an instance of an InputHandler (should be only one)
 		ih.systemEventListener = this;	//Sets the system event target to this instance
@@ -123,8 +128,9 @@ public class GameApp : SystemEventListener, InputListener {
 		adh.initAudioDevice(-1);							//Initializes the default device
 		modMan = new ModuleManager(adh);					//Initializes the module manager
 		modCfg = new ModuleConfig(modMan);					//Initializes the module configurator
-		modCfg.loadConfigFromFile("yourAudioConfiguration.sdl");//This line loads an audio configuration file (make sure you have a valid one - create one with the ADK/test1!)
+		modCfg.loadConfigFromFile(resolvePath("yourAudioConfiguration.sdl"));//This line loads an audio configuration file (make sure you have a valid one - create one with the ADK/test1!)
 		modCfg.compile(false);								//Compiles the current module configuration.
+		//MIDI 1.0 sequencer, can be replaced with the IMBC sequencer if needed
 		midiSeq = new SequencerM1(modMan.moduleList, modCfg.midiRouting, modCfg.midiGroups);
 		modMan.runAudioThread();							//Runs the audio thread.
 		//audio related part end
@@ -148,6 +154,7 @@ public class GameApp : SystemEventListener, InputListener {
 
 			//<Per-frame code comes here>
 		}
+		destroy(outputScreen);	//Make sure the destructors of our output screen run as intended.
 	}
 	///This function will load a map file to display levels, or portions of levels.
 	///If you're clever, you can store other things in map files
@@ -156,7 +163,7 @@ public class GameApp : SystemEventListener, InputListener {
 		foreach (key, elem; mapSource.layeroutput) {
 			rstr.removeLayer(key);
 		}
-		mapSource = new MapFormat(File(m));			//Loads the map file itself, and parses it.
+		mapSource = new MapFormat(File(resolvePath(m)));//Loads the map file itself, and parses it.
 		mapSource.loadTiles(rstr);					//Loads the tiles with the palettes needed to display them.
 		mapSource.loadAllSpritesAndObjects(rstr, ocd);	//Loads all sprites and objects to the layers, and the collision detector. Also loads the palettes for the sprites
 		mapSource.loadMappingData();				//Loads all mapping data to the tilelayers.
@@ -174,24 +181,44 @@ public class GameApp : SystemEventListener, InputListener {
 		//You might want to put here a more complicated prompt instead.
 		stateFlags.isRunning = false;
 	}
+	/**
+	 * Called if a window was resized.
+	 * Params:
+	 *   window = Handle to the OSWindow class.
+	 *   width = active area width.
+	 *   height = active area height.
+	 */
+	public void windowResize(OSWindow window, int width, int height) {
+		//This method is set up so if the window is being resized, the manager will try its best to keep the aspect ratio
+		//Feel free to modify it if you need some other functionality
+		immutable double origAspectRatio = cast(double)SCREEN_WIDTH / cast(double)SCREEN_HEIGHT;//Calculate original aspect ratio
+		double newAspectRatio = cast(double)width / cast(double)height;//Calculate new aspect ratio
+		if (newAspectRatio > origAspectRatio) {		//Display area is now wider, padding needs to be added on the sides
+			const double visibleWidth = height * origAspectRatio;
+			const double sideOffset = (width - visibleWidth) / 2.0;
+			glViewport(cast(int)sideOffset, 0, cast(int)visibleWidth, height);
+		} else {	//Display area is now taller, padding needs to be added on the top and bottom
+			const double visibleHeight = width / origAspectRatio;
+			const double topOffset = (height - visibleHeight) / 2.0;
+			glViewport(0, cast(int)topOffset, width, cast(int)visibleHeight);
+		}
+	}
 	///Called when a controller is added to the system.
-	///Note: This function will be changed once I move input handling and output screen handling to iota.
-	public void controllerAdded(uint id) {
-		
+	public void inputDeviceAdded(InputDevice id) {
+
 	}
 	///Called when a controller is removed the system.
-	///Note: This function will be changed once I move input handling and output screen handling to iota.
-	public void controllerRemoved(uint id) {
-		
+	public void inputDeviceRemoved(InputDevice id) {
+
 	}
 	///Called if a key input event has occured.
 	///Note: This function will be changed once I move input handling and output screen handling to iota.
-	public void keyEvent(uint id, BindingCode code, uint timestamp, bool isPressed) {
+	public void keyEvent(uint id, BindingCode code, Timestamp timestamp, bool isPressed) {
 		
 	}
 	///Called if an axis input event has occured.
 	///Note: This function will be changed once I move input handling and output screen handling to iota.
-	public void axisEvent(uint id, BindingCode code, uint timestamp, float value) {
+	public void axisEvent(uint id, BindingCode code, Timestamp timestamp, float value) {
 		
 	}
 }
