632d84a22ecd993aa9090882fea5cfca6e251beb
ZILtoid1991 <laszloszeremi@outlook.com>
2025-03-23 23:17:49 +0100

    Added intrinsic for converting 2 ints to a double vector

--- pixelperfectengine/src/pixelperfectengine/system/intrinsics.d
+++ pixelperfectengine/src/pixelperfectengine/system/intrinsics.d
@@ -33,6 +33,10 @@ __m128i _vect(int[4] arg) @nogc @trusted pure nothrow {
 	return _mm_loadu_si32(arg.ptr);
 }
 pragma(inline, true)
+__m128d _conv2ints(int arg0, int arg1) @nogc @trusted pure nothrow {
+	return _mm_cvtpi32_pd(arg0 | (arg1<<32L));
+}
+pragma(inline, true)
 __m128d _conv2shorts(short* memAddr) @nogc @trusted pure nothrow {
 	__m128i workpad;
 	workpad[0] = memAddr[0];
