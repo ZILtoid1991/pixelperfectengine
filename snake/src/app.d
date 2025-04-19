e33644eb364db5dad1daf33358084eef53411feb
ZILtoid1991 <laszloszeremi@outlook.com>
2025-04-20 00:52:18 +0200

    Reworking the snake game for the new API
    hiss siss
    this is snek

--- snake/src/app.d
+++ snake/src/app.d
@@ -2,6 +2,8 @@ module snake.app;
 
 import std.algorithm;
 
+import std.stdio;
+
 import bindbc.opengl;
 
 import pixelperfectengine.graphics.raster;
@@ -35,7 +37,6 @@ int main(string[] args) {
 }
 /** 
  * A simple snake game.
- * Uses code-generated graphics to avoid relying on external assets.
  *
  * Game rules:
  * * The player controls a snake.
@@ -45,11 +46,11 @@ int main(string[] args) {
  * * If the player somehow makes the snake to grow so big a new apple cannot be spawned, then the player wins.
  *
  * Design guidelines:
- * * No much external assets. (except for engine essentials).
+ * * Minimal asset usage (current version uses a tilemap, previous one only had engine related ones)
  * * Snake tail follows past directions
  *
  * Some possible assignments to practice using the engine:
- * * Use some external assets.
+ * * Use some other assets.
  * * Add some obstacles.
  * * Add a second, third, etc. player.
  */
@@ -108,117 +109,38 @@ public class SnakeGame : InputListener, SystemEventListener {
 	/// * Displays the current game state
 	/// * Stores playfield data (snaketail, apple's position, etc)
 	TileLayer		playfield;
-	///These are the tiles, we're going to use in our game
-	Bitmap8Bit		empty, apple, snakeH, snakeV, snakeNE, snakeSE, snakeNW, snakeSW;
 	///A random number generator
 	RandomNumberGenerator rng;
 	///Constructor to initialize things.
 	///It is important to keep initialization and game logic separate.
 	public this() {
-		//Create all the tiles needed for the game.
 
-		//Let's make empty tiles a grid!.
-		empty = new Bitmap8Bit(8,8);
-		for (int y ; y < 8 ; y+=2) {
-			empty.writePixel(0, y, 0x1);
-			empty.writePixel(7, y + 1, 0x1);
-		}
-		for (int x ; x < 8 ; x+=2) {
-			empty.writePixel(x, 0, 0x1);
-			empty.writePixel(x + 1, 7, 0x1);
-		}
-		//The apple uses all red color. Initialize an empty bitmap, then set all the pixels to index 2.
-		apple = new Bitmap8Bit(8,8);
-		for (int y ; y < 8 ; y++) {	//Use some iterations to easily set all indexes. Top-left is the origin point, and indexing starts at 0.
-			for (int x ; x < 8 ; x++) {
-				//NOTE: it's faster to write to the bitmap by directly accessing its data, but for this purpose, it's
-				//more than enough.
-				apple.writePixel(x, y, 0x2);
-			}
-		}
-		//The snakeH is a horizontal piece, and our snake is 4 pixels thick. Let's draw it with similar method!
-		snakeH = new Bitmap8Bit(8,8);
-		for (int y = 2 ; y < 6 ; y++) {
-			for (int x ; x < 8 ; x++) {
-				snakeH.writePixel(x, y, 0x1);
-			}
-		}
-		//The snakeV is the straight vertical piece.
-		snakeV = new Bitmap8Bit(8,8);
-		for (int y ; y < 8 ; y++) {
-			for (int x = 2 ; x < 6 ; x++) {
-				snakeV.writePixel(x, y, 0x1);
-			}
-		}
-		//The corner pieces are more complicated, but not too hard.
-		//This piece is the north-east piece, it connects the top (north) with the right (east). The rest of the corner
-		//tiles use the same system, so I won't annotate their creation.
-		snakeNE = new Bitmap8Bit(8,8);
-		for (int y ; y < 2 ; y++) {
-			for (int x = 2 ; x < 6 ; x++) {
-				snakeNE.writePixel(x, y, 0x1);
-			}
-		}
-		for (int y = 2 ; y < 6 ; y++) {
-			for (int x = 2 ; x < 8 ; x++) {
-				snakeNE.writePixel(x, y, 0x1);
-			}
-		}
-		snakeSE = new Bitmap8Bit(8,8);
-		for (int y = 6 ; y < 8 ; y++) {
-			for (int x = 2 ; x < 6 ; x++) {
-				snakeSE.writePixel(x, y, 0x1);
-			}
-		}
-		for (int y = 2 ; y < 6 ; y++) {
-			for (int x = 2 ; x < 8 ; x++) {
-				snakeSE.writePixel(x, y, 0x1);
-			}
-		}
-		snakeNW = new Bitmap8Bit(8,8);
-		for (int y ; y < 2 ; y++) {
-			for (int x = 2 ; x < 6 ; x++) {
-				snakeNW.writePixel(x, y, 0x1);
-			}
-		}
-		for (int y = 2 ; y < 6 ; y++) {
-			for (int x ; x < 6 ; x++) {
-				snakeNW.writePixel(x, y, 0x1);
-			}
-		}
-		snakeSW = new Bitmap8Bit(8,8);
-		for (int y = 6 ; y < 8 ; y++) {
-			for (int x = 2 ; x < 6 ; x++) {
-				snakeSW.writePixel(x, y, 0x1);
-			}
-		}
-		for (int y = 2 ; y < 6 ; y++) {
-			for (int x ; x < 6 ; x++) {
-				snakeSW.writePixel(x, y, 0x1);
-			}
-		}
 
-		//We now have all the necessary graphics elements, let's create the rest!
 		//First, we need an output screen. It's 4:3, which isn't very modern, but will be useful for demo purposes.
 		output = new OSWindow("Snek game", "ppe_snekgaem", -1, -1, 320 * 4, 240 * 4, WindowCfgFlags.IgnoreMenuKey);
 		//Initialize OpenGL.
-		output.getOpenGLHandle();
+		version (Windows) output.getOpenGLHandleAttribsARB([
+			OpenGLContextAtrb.MajorVersion, 3,
+			OpenGLContextAtrb.MinorVersion, 3,
+			OpenGLContextAtrb.ProfileMask, 1,
+			OpenGLContextAtrb.Flags, OpenGLContextFlags.Debug,
+			0
+		]);
+		else output.getOpenGLHandle();
 		//Load OpenGL.
 		const glStatus = loadOpenGL();
-		assert(glStatus >= GLSupport.gl11, "OpenGL not found!");
+		assert(glStatus >= GLSupport.gl33, "OpenGL not found!");
 		
 		//Next, we have to create the raster, with 320x240 resolution, and 256 colors. Technically we will only use 3 
 		//colors, however one should overprovision the palette length to the multiple of the maximum color of the used
 		//indexed bitmap. (16 bit is mainly there to directly access all colors of the palette)
-		raster = new Raster(320, 240, output, 256);
-		//Set up the color palette
-		raster.setPaletteIndex(0, Color(0,0,0,255));
-		raster.setPaletteIndex(1, Color(255,255,255,255));
-		raster.setPaletteIndex(2, Color(255,0,0,255));
+		raster = new Raster(320, 240, output);
+		raster.readjustViewport(320 * 4, 240 * 4, 0, 0);
+
 		//Let's create our playfield, where the action will happen.
-		//The `Copy` rendering mode does not support transparency or anything fancy like that, but is very fast and
-		//harder to make things go bad.
-		playfield = new TileLayer(8, 8, RenderingMode.Copy);
+		//The default tile rendering shader will be good enough.
+		playfield = new TileLayer(8, 8, GLShader(loadShader(`%SHADERS%/tile_%SHDRVER%.vert`),
+				loadShader(`%SHADERS%/tile_%SHDRVER%.frag`)));
 		//Add the tile layer to the raster.
 		raster.addLayer(playfield, 0);
 		//Load an empty mapping into our playfield, which we will be modifying later through the `readMapping` and 
@@ -228,15 +150,21 @@ public class SnakeGame : InputListener, SystemEventListener {
 			emptyMap.length = 40 * 30;
 			playfield.loadMapping(40, 30, emptyMap);
 		}
+		//Load the source of the tilemap and the palette
+		Image tilemap = loadImage(File(resolvePath("%PATH%/assets/snake-game-tileset.png")));
+		//Load the palette from it
+		raster.loadPaletteChunk(loadPaletteFromImage(tilemap), 0);
+		//Load the tilemap into the layer
+		playfield.addBitmapSource(loadBitmapFromImage!Bitmap8Bit(tilemap), 0);
 		//Add the tiles to the playfield
-		playfield.addTile(empty, TileTypes.Empty);
-		playfield.addTile(apple, TileTypes.Apple);
-		playfield.addTile(snakeH, TileTypes.SnakeH);
-		playfield.addTile(snakeV, TileTypes.SnakeV);
-		playfield.addTile(snakeNE, TileTypes.SnakeNE);
-		playfield.addTile(snakeSE, TileTypes.SnakeSE);
-		playfield.addTile(snakeNW, TileTypes.SnakeNW);
-		playfield.addTile(snakeSW, TileTypes.SnakeSW);
+		playfield.addTile(TileTypes.Empty, 0, 0, 0);
+		playfield.addTile(TileTypes.Apple, 0, 8, 0);
+		playfield.addTile(TileTypes.SnakeH, 0, 8, 16);
+		playfield.addTile(TileTypes.SnakeV, 0, 16, 16);
+		playfield.addTile(TileTypes.SnakeNE, 0, 24, 16);
+		playfield.addTile(TileTypes.SnakeSE, 0, 32, 16);
+		playfield.addTile(TileTypes.SnakeNW, 0, 40, 16);
+		playfield.addTile(TileTypes.SnakeSW, 0, 48, 16);
 		//Initialize input handler
 		ih = new InputHandler();
 		ih.systemEventListener = this;
@@ -263,7 +191,7 @@ public class SnakeGame : InputListener, SystemEventListener {
 			if (state == 5)
 				timer.test();
 			ih.test();
-			raster.refresh();
+			raster.refresh_GL();
 			rng.seed();
 		}
 		destroy(output);
@@ -275,7 +203,8 @@ public class SnakeGame : InputListener, SystemEventListener {
 		for (int yi ; yi < 30 ; yi++) {
 			for (int xi ; xi < 40 ; xi++) {
 				if (playfield.readMapping((xi + x) % 40, (yi + y) % 30).tileID == TileTypes.Empty) {
-					playfield.writeMapping((xi + x) % 40, (yi + y) % 30, MappingElement(TileTypes.Apple));
+					playfield.writeMapping((xi + x) % 40, (yi + y) % 30, MappingElement2(TileTypes.Apple));
+					playfield.reprocessTilemap();
 					return;
 				}
 			}
@@ -306,7 +235,7 @@ public class SnakeGame : InputListener, SystemEventListener {
 		}
 		curr = snakeHead;
 		// Check for collision
-		MappingElement me = playfield.readMapping(curr.x, curr.y);
+		MappingElement2 me = playfield.readMapping(curr.x, curr.y);
 		if (me.tileID == TileTypes.Apple) {	//Player has collected an apple, let's reward them!
 			score++;
 			placeNextApple();
@@ -318,14 +247,14 @@ public class SnakeGame : InputListener, SystemEventListener {
 		}
 		// Draw the head to the new place
 		if (dir == Direction.East || dir == Direction.West)
-			playfield.writeMapping(curr.x, curr.y, MappingElement(TileTypes.SnakeH));
+			playfield.writeMapping(curr.x, curr.y, MappingElement2(TileTypes.SnakeH));
 		else
-			playfield.writeMapping(curr.x, curr.y, MappingElement(TileTypes.SnakeV));
+			playfield.writeMapping(curr.x, curr.y, MappingElement2(TileTypes.SnakeV));
 		// Move the snake.
 		// This iteration finds the endpiece of the snake, by using the shape identifier codes.
 		for (int i ; i <= score ; i++) {
 			//MappingElement currT = playfield.readMapping(curr.x, curr.y);
-			MappingElement prevT = playfield.readMapping(prev.x, prev.y);
+			MappingElement2 prevT = playfield.readMapping(prev.x, prev.y);
 			switch (prevT.tileID) {
 				case TileTypes.SnakeH:
 					const int cX = curr.x, pX = prev.x;
@@ -382,15 +311,17 @@ public class SnakeGame : InputListener, SystemEventListener {
 		
 		// Remove end if growth doesn't happen.
 		if (!appleFound) 
-			playfield.writeMapping(curr.x, curr.y, MappingElement(TileTypes.Empty));
+			playfield.writeMapping(curr.x, curr.y, MappingElement2(TileTypes.Empty));
+		playfield.reprocessTilemap();
 	}
 	/// Since tilemaps get initialized with 0xFFFF (none) tiles, we need to set it to 0x0000
 	public void clearTilemap() {
 		for (int y ; y < 30 ; y++) {
 			for (int x ; x < 40 ; x++) {
-				playfield.writeMapping(x,y, MappingElement(TileTypes.Empty));
+				playfield.writeMapping(x,y, MappingElement2(TileTypes.Empty));
 			}
 		}
+		playfield.reprocessTilemap();
 	}
 	/// A timer event, to make the game run stable regardless of the framerate.
 	public void timerEvent(Duration jitter) {
@@ -408,10 +339,10 @@ public class SnakeGame : InputListener, SystemEventListener {
 				case Direction.North:
 					switch (prevDir) {
 						case Direction.East:
-							playfield.writeMapping(snakeHead.x, snakeHead.y, MappingElement(TileTypes.SnakeNW));
+							playfield.writeMapping(snakeHead.x, snakeHead.y, MappingElement2(TileTypes.SnakeNW));
 							break;
 						case Direction.West:
-							playfield.writeMapping(snakeHead.x, snakeHead.y, MappingElement(TileTypes.SnakeNE));
+							playfield.writeMapping(snakeHead.x, snakeHead.y, MappingElement2(TileTypes.SnakeNE));
 							break;
 						default: break;
 					}
@@ -419,10 +350,10 @@ public class SnakeGame : InputListener, SystemEventListener {
 				case Direction.South:
 					switch (prevDir) {
 						case Direction.East:
-							playfield.writeMapping(snakeHead.x, snakeHead.y, MappingElement(TileTypes.SnakeSW));
+							playfield.writeMapping(snakeHead.x, snakeHead.y, MappingElement2(TileTypes.SnakeSW));
 							break;
 						case Direction.West:
-							playfield.writeMapping(snakeHead.x, snakeHead.y, MappingElement(TileTypes.SnakeSE));
+							playfield.writeMapping(snakeHead.x, snakeHead.y, MappingElement2(TileTypes.SnakeSE));
 							break;
 						default: break;
 					}
@@ -430,10 +361,10 @@ public class SnakeGame : InputListener, SystemEventListener {
 				case Direction.West:
 					switch (prevDir) {
 						case Direction.North:
-							playfield.writeMapping(snakeHead.x, snakeHead.y, MappingElement(TileTypes.SnakeSW));
+							playfield.writeMapping(snakeHead.x, snakeHead.y, MappingElement2(TileTypes.SnakeSW));
 							break;
 						case Direction.South:
-							playfield.writeMapping(snakeHead.x, snakeHead.y, MappingElement(TileTypes.SnakeNW));
+							playfield.writeMapping(snakeHead.x, snakeHead.y, MappingElement2(TileTypes.SnakeNW));
 							break;
 						default: break;
 					}
@@ -441,10 +372,10 @@ public class SnakeGame : InputListener, SystemEventListener {
 				case Direction.East:
 					switch (prevDir) {
 						case Direction.North:
-							playfield.writeMapping(snakeHead.x, snakeHead.y, MappingElement(TileTypes.SnakeSE));
+							playfield.writeMapping(snakeHead.x, snakeHead.y, MappingElement2(TileTypes.SnakeSE));
 							break;
 						case Direction.South:
-							playfield.writeMapping(snakeHead.x, snakeHead.y, MappingElement(TileTypes.SnakeNE));
+							playfield.writeMapping(snakeHead.x, snakeHead.y, MappingElement2(TileTypes.SnakeNE));
 							break;
 						default: break;
 					}
@@ -452,6 +383,7 @@ public class SnakeGame : InputListener, SystemEventListener {
 				default: break;
 			}
 			prevDir = dir;
+			playfield.reprocessTilemap();
 		}
 	}
 	/// Key event data is received here.
@@ -462,7 +394,8 @@ public class SnakeGame : InputListener, SystemEventListener {
 				dir = Direction.North;
 			}
 			if (!prevDir) {
-				playfield.writeMapping(snakeHead.x, snakeHead.y, MappingElement(TileTypes.SnakeV));
+				playfield.writeMapping(snakeHead.x, snakeHead.y, MappingElement2(TileTypes.SnakeV));
+				playfield.reprocessTilemap();
 			}
 			break;
 		case hashCalc("down"):
@@ -470,7 +403,8 @@ public class SnakeGame : InputListener, SystemEventListener {
 				dir = Direction.South;
 			}
 			if (!prevDir) {
-				playfield.writeMapping(snakeHead.x, snakeHead.y, MappingElement(TileTypes.SnakeV));
+				playfield.writeMapping(snakeHead.x, snakeHead.y, MappingElement2(TileTypes.SnakeV));
+				playfield.reprocessTilemap();
 			}
 			break;
 		case hashCalc("left"):
@@ -492,7 +426,8 @@ public class SnakeGame : InputListener, SystemEventListener {
 			dir = 0;
 			prevDir = 0;
 			placeNextApple();
-			playfield.writeMapping(snakeHead.x, snakeHead.y, MappingElement(TileTypes.SnakeH));
+			playfield.writeMapping(snakeHead.x, snakeHead.y, MappingElement2(TileTypes.SnakeH));
+			playfield.reprocessTilemap();
 			break;
 		case hashCalc("quit"):
 			state = 4;
@@ -532,11 +467,11 @@ public class SnakeGame : InputListener, SystemEventListener {
 		if (newAspectRatio > origAspectRatio) {		//Display area is now wider, padding needs to be added on the sides
 			const double visibleWidth = height * origAspectRatio;
 			const double sideOffset = (width - visibleWidth) / 2.0;
-			glViewport(cast(int)sideOffset, 0, cast(int)visibleWidth, height);
+			raster.readjustViewport(cast(int)visibleWidth, height, cast(int)sideOffset, 0);
 		} else {	//Display area is now taller, padding needs to be added on the top and bottom
 			const double visibleHeight = width / origAspectRatio;
 			const double topOffset = (height - visibleHeight) / 2.0;
-			glViewport(0, cast(int)topOffset, width, cast(int)visibleHeight);
+			raster.readjustViewport(width, cast(int)visibleHeight, 0, cast(int)topOffset);
 		}
 	}
 }
