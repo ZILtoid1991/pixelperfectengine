ae94d139562c60f32dba0723db743eb4c3db1988
ZILtoid1991 <laszloszeremi@outlook.com>
2025-07-23 21:01:54 +0200

    Upgrading of an old file to current specs

--- pixelperfectengine/src/pixelperfectengine/system/dictionary.d
+++ pixelperfectengine/src/pixelperfectengine/system/dictionary.d
@@ -6,34 +6,40 @@ module pixelperfectengine.system.dictionary;
  * Pixel Perfect Engine, dictionary module
  */
 import newsdlang;
-import std.stdio;
 /**
  * Does a two-way coding based on an SDLang file.
  */
 public class Dictionary{
-	private string[int] encodeTable;
-	private int[string] decodeTable;
+	// private string[int] encodeTable;
+	// private int[string] decodeTable;
+	private string[] tableA;
+	private int[] tableB;
 	/// Uses tag values (string, int) to generate the dictionary
-	public this(DLDocument root){
-		try{
+	public this(DLTag root) {
+		try {
 			//Tag root = parseFile(filename);
-			foreach(DLTag t; root.tags){
+			foreach(DLTag t; root.tags) {
 				string s = t.values[0].get!string();
 				int i = t.values[1].ge!int();
-				encodeTable[i] = s;
-				decodeTable[s] = i;
+				tableA ~= s;
+				tableB ~= i;
 			}
-		}
-		catch(ParseException e){
+		} catch(ParseException e) {
 			debug writeln(e.msg);
 		}
 	}
 	/// Returns the first value from the encodeTable, where decodeTable[i] == input
-	public string encode(int input){
-		return encodeTable.get(input, null);
+	public string encode(int input) {
+		for (size_t i ; i < tableA.length ; i++) {
+			if (tableB[i] == input) return tableA[i];
+		}
+		return null;
 	}
 	/// Returns the first value from the decodeTable, where encodetable[i] == input
-	public int decode(string input){
-		return decodeTable.get(input, -1);
+	public int decode(string input) {
+		for (size_t i ; i < tableA.length ; i++) {
+			if (tableA[i] == input) return tableB[i];
+		}
+		return -1;
 	}
 }
