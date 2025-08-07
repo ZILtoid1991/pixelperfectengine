dfc554e3dc52903eb3955829019042a599c5c380
ZILtoid1991 <laszloszeremi@outlook.com>
2025-08-07 23:22:38 +0200

    File refactoring

--- pixelperfectengine/src/pixelperfectengine/physics/collision.d
+++ pixelperfectengine/src/pixelperfectengine/physics/collision.d
@@ -1,15 +1,17 @@
-module pixelperfectengine.physics.common;
+module pixelperfectengine.physics.collision;
 
 /*
  * Copyright (C) 2015-2020, 2025, by Laszlo Szeremi under the Boost license.
  *
- * Pixel Perfect Engine, physics.common module.
+ * Pixel Perfect Engine, physics.collision module.
  */
 
 public import pixelperfectengine.graphics.common;
 public import pixelperfectengine.graphics.bitmap;
 public import pixelperfectengine.graphics.layers : MappingElement2;
-import collections.treemap;
+import pixelperfectengine.system.ecs;
+package import pixelperfectengine.system.memory;
+// import collections.treemap;
 
 /**
  * Defines a shape for collision detection.
@@ -17,23 +19,25 @@ import collections.treemap;
 public struct CollisionShape {
 	Box				position;	///Position of the shape in the 2D space.
 	Bitmap1Bit		shape;		///The shape defined by a 1 bit bitmap. Null if custom shape isn't needed
+	mixin(ECS_MACRO);
 	/**
 	 * Creates a collision shape.
 	 * Params:
 	 *   position: The position of the bounding box.
 	 *   shape: The shape of the object in the form of a 1 bit bitmap if any, null otherwise.
 	 */
-	public this(Box position, Bitmap1Bit shape) @nogc @safe pure nothrow {
+	public this(int ecsID, Box position, Bitmap1Bit shape) @nogc @safe pure nothrow {
+		this.ecsID = ecsID;
 		this.position = position;
 		this.shape = shape;
 		//this.id = id;
 	}
 }
-alias ObjectMap = TreeMap!(int, CollisionShape);
+alias ObjectList = OrderedArraySet!CollisionShape;
 /**
  * Contains information about an object collision event.
  */
-public class ObjectCollisionEvent {
+public struct ObjectCollisionEvent {
 	/**
 	 * Defines types of object collisions that can happen
 	 */
@@ -49,8 +53,11 @@ public class ObjectCollisionEvent {
 	int				idA;		///ID of object A
 	int				idB;		///ID of object B
 	int				contextID;	///The context of the collision (e.g. tester ID)
-	Box				overlap;	///Overlapping area of the collision
 	Type			type;		///Type of the object collision
+	Box				overlap;	///Overlapping area of the collision
+	static ObjectCollisionEvent nullCollision() @nogc @safe pure nothrow {
+		return ObjectCollisionEvent(null, null, 0, Box.init, Type.None);
+	}
 	///default CTOR
 	public this(CollisionShape* shA, CollisionShape* shB, int contextID, Box overlap, Type type) 
 			@nogc @safe pure nothrow {
